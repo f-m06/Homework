a1053743e352b54dc40c637b4537bc3bf05e41c1
Farhan Miah <farhanhmiah18@gmail.com>
2023-02-03 09:59:25 +0000

    Unit converter draft

---
+++ 06 Unit Converter/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Homework
+{
+    class Program
+    {
+        static void Temperature()
+        {
+            ;
+            static void CelsiusToFahrenheit()
+            {
+                ;
+            }
+            static void FahrenheitToCelsius()
+            {
+                ;
+            }
+        }
+        static void Currency()
+        {
+            ;
+            static void GBPToUSD()
+            {
+                ;
+            }
+            static void GBPToEUR()
+            {
+                ;
+            }
+            static void USDToGBP()
+            {
+                ;
+            }
+            static void USDToEUR()
+            {
+                ;
+            }
+            static void EURToGBP()
+            {
+                ;
+            }
+            static void EURToUSD()
+            {
+                ;
+            }
+        }
+        static void Volume()
+        {
+            ;
+            static void CubicMetreToCubicFoot()
+            {
+                ;
+            }
+            static void CubicFootToCubicMetre()
+            {
+                ;
+            }
+        }
+        static void Main(String[] args)
+        {
+            String UserInput = "";
+            Console.WriteLine("~~~~~~~~~~");
+            while (UserInput != "end")
+            {
+                Console.WriteLine("Enter a type of measurement\nTemperature\nCurrency\nVolume\nEnter end to terminate the program\n~~~~~~~~~~");
+                UserInput = Console.ReadLine();
+                if (UserInput.ToLower() == "temperature")
+                {
+                    Temperature();
+                }
+                else if (UserInput.ToLower() == "currency")
+                {
+                    Currency();
+                }
+                else if (UserInput.ToLower() == "volume")
+                {
+                    Volume();
+                }
+            }
+        }
+    }
+}
